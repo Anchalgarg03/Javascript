d66f56ce91d7e9d20e6842eb3fb58adb0ceee934
Anchalgarg03 <anchalgarg859@gmail.com>
2026-06-26 19:03:53 +0000

    variables of javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const account_ID = 1445566 //cannot change the value of const
+let account_email = "anchal@google.com"
+var accountPass = "12345"
+accountcity = "Jaipur"
+console.log(account_ID);
+account_email = "ag&ag.com"
+accountPass = "1212"
+accountcity = "Meerut"
+let acc_state;
+console.log(account_email);
+console.table([account_ID,account_email,accountPass,accountcity,accountPass])
+
+/* prefer not to use var because of use in blog scope and functional scpoe*/
