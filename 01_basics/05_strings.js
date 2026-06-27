7bc79d901f37146c00643d2af53f8e59018b9646
Anchalgarg03 <anchalgarg859@gmail.com>
2026-06-27 13:31:14 +0000

    changes

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,22 @@
+let name = "Anchal"
+let count = 50
+console.log(name + count + "value");
+console.log(`Hello my name is ${name}`);
+console.log(name[0]);
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.charAt(2));
+console.log(name.indexOf('c'));
+const newstr = name.substring(0,4)
+console.log(newstr);
+const new_str = "  anchal  "
+console.log(new_str);
+console.log(new_str.trim());
+const url = "https://anchal.com/anchal%20garg"
+console.log(url.replace('%20',''));
+
+ 
+
+
+
+
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,18 @@
+const arr = [0,1,2,34,5,6,true,"anchal"] 
+console.log(arr);
+arr.push(10)
+console.log(arr);
+arr.pop()
+
+arr.unshift(0)
+console.log(arr);
+console.log(arr.includes(5));
+console.log(arr.indexOf(34));
+const new_arr = arr.join()
+console.log(new_arr);
+
+
+
+
+
+
