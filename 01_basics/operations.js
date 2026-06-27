2acb6f5c89ea2d1508d8959bfbb4da40f0b21372
Anchalgarg03 <anchalgarg859@gmail.com>
2026-06-27 07:07:29 +0000

    about datatypes

---
+++ 01_basics/operations.js
@@ -0,0 +1,53 @@
+//operations
+let value = 3
+let negvalue = -value
+/*console.log(value)
+console.log(negvalue)
+console.log(1+"2")
+console.log("1"+2+2);
+console.log(1+2+"2");
+*/
+/*let counter = 100
+counter++
+console.log(counter);
+//console.log("2">1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);*/
+
+//DATATYPES SUMMARY
+const score = 100
+const val = 100.3
+const isLoggedIn = false
+const temp = null
+let user_email;
+const id = Symbol('123')
+const another_id = Symbol('123')
+console.log(id===another_id);
+
+const bigno = 123456987
+
+//non primitive
+const heros = ["shaktiman","naagraj","doga"]
+let my_obj = {
+    name: "anchal",
+    age: 18,
+
+}
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+console.log(typeof bigno);
+
+console.log(typeof myFunction);
+
+console.log(typeof my_obj);
+
+console.log(typeof heros);
+
+
+
+
+
+
