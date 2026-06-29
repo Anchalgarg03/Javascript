f010d339d9fb29d4202473cbd1b5974b4ec2daaf
Anchalgarg03 <anchalgarg859@gmail.com>
2026-06-29 20:23:53 +0000

    hyuhu

--- 01_f/basic.js
+++ 01_f/basic.js
@@ -3,3 +3,17 @@ function add(num1,num2){
     
 }
 add(5,25)
+//rest operator 
+function rest(...num1){
+    return num1
+}
+console.log(rest(200,300,400,256,859));
+ const user = {
+    username: "anchal",
+    age: 18
+ }
+ function handleuser(anyobject){
+    console.log(`my name is ${anyobject.username} and I am ${anyobject.age} years old`);
+    
+ }
+ handleuser(user)
